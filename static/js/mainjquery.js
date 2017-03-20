93062d29de21a9dfdaae98af30e0ba94a05893ba
dry <szarazdenes@gmail.com>
2017-03-20 19:36:13 +0100

    adding new block without existing one

--- static/js/mainjquery.js
+++ static/js/mainjquery.js
@@ -2,7 +2,13 @@
  * Created by dry on 2017. 03. 20..
  */
 $(document).ready(function () {
-    $("#cloner").click(function () {
-        $("#duplicate").clone().appendTo("#filler:last");
+    var boardcard = '<div class="col-lg-3 col-md-4 col-xs-12 board"><p>Helloodasdad</p><button>save</button></div>';
+
+
+    $("#adder").click(function () {
+        $("#filler").append(boardcard);
     });
 });
+
+
+
