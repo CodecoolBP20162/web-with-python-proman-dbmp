35766c37edc239905dcd6cd873fcfd1ec1ce9c10
dry <szarazdenes@gmail.com>
2017-03-21 20:27:37 +0100

    whole restructure

--- static/js/mainjquery.js
+++ static/js/mainjquery.js
@@ -2,33 +2,16 @@
  * Created by dry on 2017. 03. 20..
  */
 $(document).ready(function () {
-    var $template = $("#board_template")
-    var counter = 0;
-    $template.hide();
 
-    $("#adder").click(function () {
-        $template.clone().attr("id", "board" + (++counter)).appendTo("#filler").show()
-        $("#filler").sortable({
-            connectWith: "#filler",
-            revert: true,
-            dropOnEmpty: true
-        })
-        ;
+    $("#add_card").click(function () {
 
-        $(".card-entry").sortable({
-            connectWith: ".card-entry",
-            revert: true,
-            dropOnEmpty: true
-        })
-        $("#board" + counter + " .add_card").click(function () {
-            var cardName = $('#board' + counter + ' input[name=card-name]').val();
-            $('#board' + counter + " .card-entry").append("<li class='dragentries'><p>" + cardName + "</p></li>");
-
-
-        });
-
+        var cardName = $("#input-id").val();
+        $("#newcollector").append("<li class='dragentries'><p>" + cardName + "</p></li>");
 
     });
+
+
+     $(".card-entry").sortable({connectWith: ".card-entry", revert: true, dropOnEmpty: true})
 
 });
 
