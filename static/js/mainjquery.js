4657a7746975681b134acd2d068363e1a085b001
dry <szarazdenes@gmail.com>
2017-03-21 17:34:35 +0100

    working drag and drop

--- static/js/mainjquery.js
+++ static/js/mainjquery.js
@@ -7,10 +7,29 @@ $(document).ready(function () {
     $template.hide();
 
     $("#adder").click(function () {
-        $template.clone().attr("id", "board" + (++counter)).appendTo("#filler").show();
+        $template.clone().attr("id", "board" + (++counter)).appendTo("#filler").show()
+        $("#filler").sortable({
+            connectWith: "#filler",
+            revert: true,
+            dropOnEmpty: true
+        })
+        ;
+
+        $(".card-entry").sortable({
+            connectWith: ".card-entry",
+            revert: true,
+            dropOnEmpty: true
+        })
         $("#board" + counter + " .add_card").click(function () {
-    	    var cardName = $('#board' + counter + ' input[name=card-name]').val();
-            $('#board' + counter + " .card-entry").append("<p>"+cardName+"</p>");
+            var cardName = $('#board' + counter + ' input[name=card-name]').val();
+            $('#board' + counter + " .card-entry").append("<li class='dragentries'><p>" + cardName + "</p></li>");
+
+
         });
+
+
     });
+
 });
+
+
