92f18a306662f382c57a352786279bc1bb866e6e
Balázs Máté <balazsm86@gmail.com>
2017-03-23 10:42:10 +0100

    Formatting code

--- static/js/main.js
+++ static/js/main.js
@@ -11,38 +11,26 @@ function BoardClass(BoardId, BoardName) {
 }
 
 function CardLoader() {
-
-
     let retrievedCardObject = JSON.parse(localStorage.getItem("CardCollector"));
 
-
     for (let i = 0; i < retrievedCardObject.length; i++) {
-
-          let AppendableEntry = "<li class='dragentries'><p>" + retrievedCardObject[i].CardText + "</p><div class='trash' id='delete" + retrievedCardObject[i].CardId + "'></div><div class='edit' id='edit" + retrievedCardObject[i].CardId + "'></div></li>";
-
+        let AppendableEntry = "<li class='dragentries'><p>" + retrievedCardObject[i].CardText + "</p><div class='trash' id='delete" + retrievedCardObject[i].CardId + "'></div><div class='edit' id='edit" + retrievedCardObject[i].CardId + "'></div></li>";
 
         if (retrievedCardObject[i].CardState === "new") {
-
             $("#newcollector").append(AppendableEntry);
-
-
         }
 
         else if (retrievedCardObject[i].CardState === "in-progress") {
             $("#in-progesscollector").append(AppendableEntry);
-
         }
 
         else if (retrievedCardObject[i].CardState === "review") {
             $("#reviewcollector").append(AppendableEntry);
-
         }
 
         else if (retrievedCardObject[i].CardState === "done") {
             $("#donecollector").append(AppendableEntry);
-
         }
-
     }
 }
 
@@ -55,31 +43,23 @@ function LocalInit() {
 }
 
 function CardAdder() {
-
     let CardCounter = 1;
 
     LocalInit();
 
     let CardCollector = JSON.parse(localStorage.getItem("CardCollector"));
 
-
     $("#add-card").click(function () {
-
         let CardObject = new CardClass(CardCounter, $("#card-input").val(), "new", 1);
-
         CardCollector.push(CardObject);
 
         $("#newcollector").append("<li class='dragentries'><p>" + CardObject.CardText + "</p><div class='trash' id='delete" + CardObject.CardId + "'></div><div class='edit' id='edit" + CardObject.CardId + "'></div></li>");
-
         localStorage.setItem('CardCollector', JSON.stringify(CardCollector));
-
-        CardCounter++
+        CardCounter++;
     });
 }
 
 function CardDragger() {
-
-
     $(".card-entry").sortable({
         connectWith: ".card-entry",
         revert: true,
@@ -87,31 +67,22 @@ function CardDragger() {
     })
 }
 
-
 function BoardAdder() {
-
     let BoardCounter = 1;
-
     let BoardCollector = [];
 
     $("#add-board").click(function () {
-
         let BoardObject = new BoardClass(BoardCounter, $("#board-input").val());
-
         BoardCollector.push(BoardObject);
-
         let AddableEntry = "<div class='dragentries col-lg-3 col-md-4 col-sm-6 col-xs-12'><div class='board-wrapper'> <p>" + BoardObject.BoardName + "</p><div class='trash' id='delete" + BoardObject.BoardId + "'></div><div class='edit' id='edit" + BoardObject.BoardId + "'></div></div></div>";
 
         $("#boardcollector").append(AddableEntry);
-
         localStorage.setItem('BoardCollector', JSON.stringify(BoardCollector));
-
-        BoardCounter++
+        BoardCounter++;
     });
 }
 
 function BoardDragger() {
-
     $(".board-entry").sortable({
         connectWith: ".card-entry",
         revert: true,
@@ -121,15 +92,9 @@ function BoardDragger() {
 
 
 $(document).ready(function () {
-
     CardDragger();
-
     BoardDragger();
-
     CardAdder();
-
     BoardAdder();
-
     CardLoader();
-
 });
